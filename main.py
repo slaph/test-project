82179a7d6b51c2a853ed8b10764c10521ac6ef2d
slaph <seregakiselev84@gmail.com>
2020-11-14 13:05:11 +0300

    Include 'threading'

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
-
 import random
 import time
-from test_git_r import shikilist
-from test_git_r.api import MessageSend
+from test_git import shikilist
+from test_git.api import MessageSend
 from datetime import datetime
-
+import threading
 Api = MessageSend()
 rps = 0
 
 
 def object_find_and_send(for_id, rpS, message, attc):
-    if int(for_id) > 2000000000: #только для беседы
+    if int(for_id) > 2000000000:  # только для беседы
 
         if message.startswith(('добавить', 'в список', 'тайтл')) and rpS != 90:
             input_message1 = message.split()
             if input_message1[0] == 'тайтл':
-                listtodo = [input_message1[0], input_message1[1], '%20'.join(input_message1[2:])]
+                # listtodo = [input_message1[0], input_message1[1], '%20'.join(input_message1[2:])]
 
-                test = shikilist.ItemTitles(listtodo)
+                # test = shikilist.ItemTitles(listtodo)
 
-                info, titleInMy, cell_id = test.getTitle()
+                # info, titleInMy, cell_id = test.getTitle()
 
-                inf_about_title = [*info['name'], str(info['description']), *list(titleInMy.values())]
+                # inf_about_title = [*info['name'], str(info['description']), *list(titleInMy.values())]
 
-                names = ['chat_id', 'message', 'random_id'] # для ответа в лс должно быть peer_id
+                # names = ['chat_id', 'message', 'random_id'] # для ответа в лс должно быть peer_id
 
-                params = [str(for_id)[-1], '\n--------------------\n'.join(inf_about_title), random.randint(-2147483648, +2147483648)]
+                # params = [str(for_id)[-1], '\n--------------------\n'.join(inf_about_title), random.randint(-2147483648, +2147483648)]
 
-                Api.api('messages.send', names, params)
+                # Api.api('messages.send', names, params)
 
                 rpS += 5
 
@@ -37,24 +36,34 @@ def object_find_and_send(for_id, rpS, message, attc):
             Api.api('messages.send', names, params)
 
 
+def message(event):
+    print(threading.currentThread().getName())
+    start = time.time()
+    if event['type'] == 'message_new':
+        forid = event["object"]['peer_id']
+        input_message = event["object"]['text'].lower()
+        att = (event["object"])['attachments']
+        if (len(att) == 0) and (len(event["object"]['fwd_messages']) != 0):
+            object_find_and_send(forid, rps, input_message, event["object"]['fwd_messages'][0]['attachments'])
+        else:
+            object_find_and_send(forid, rps, input_message, att)
+        print(datetime.now())
+        print(time.time() - start)
+
+
 while True:
     try:
         a = Api.longPoll()
         while len(a[1]) == 0:
+            print('noevents')
             a = Api.longPoll()
         print(len(a[1]))
         event = (a[1])[0]
+        for i in range(1):
+            my_thread = threading.Thread(target=message, args=(event,))
+            my_thread.start()
 
-        start = time.time()
-        if event['type'] == 'message_new':
-            forid = event["object"]['peer_id']
-            input_message = event["object"]['text'].lower()
-            att = (event["object"])['attachments']
-            if (len(att) == 0) and (len(event["object"]['fwd_messages']) != 0):
-                object_find_and_send(forid, rps, input_message, event["object"]['fwd_messages'][0]['attachments'])
-            else:
-                object_find_and_send(forid, rps, input_message,att)
-            print(datetime.now())
-            print(time.time() - start)
+
+
     except Exception:
         print(Exception)
