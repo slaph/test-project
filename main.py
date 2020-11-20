918e6df501d1ef21e7a18d0fefd625be351108ed
slaph <seregakiselev84@gmail.com>
2020-11-21 00:15:58 +0300

    rework threading

--- main.py
+++ main.py
@@ -4,33 +4,39 @@ from test_git import shikilist
 from test_git.api import MessageSend
 from datetime import datetime
 import threading
-Api = MessageSend()
+
+api = MessageSend()
 rps = 0
+cell_id_list = []
 
 
-def object_find_and_send(for_id, rpS, message, attc):
+def object_find_and_send(for_id, rpS, current_id, taken_message, attc):
+    Api = MessageSend()
     if int(for_id) > 2000000000:  # только для беседы
 
-        if message.startswith(('добавить', 'в список', 'тайтл')) and rpS != 90:
-            input_message1 = message.split()
-            if input_message1[0] == 'тайтл':
-                # listtodo = [input_message1[0], input_message1[1], '%20'.join(input_message1[2:])]
+        if taken_message.startswith(('добавить', 'в список', 'аниме', 'манга')) and rpS != 90:
+            input_message1 = taken_message.split()
+            if input_message1[0] == 'аниме' or input_message1[0] == 'манга':
+                cell_id_list.clear()
+                listtodo = [input_message1[0], '%20'.join(input_message1[1:])]
 
-                # test = shikilist.ItemTitles(listtodo)
+                test = shikilist.ItemTitles(listtodo)
 
-                # info, titleInMy, cell_id = test.getTitle()
+                info, titleInMy, cell_id, titleId = test.getTitle()
+                current_id['object'] = titleId
+                inf_about_title = [*info['name'], str(info['description']), *list(titleInMy.values())]
+                cell_id_list.append(cell_id)
+                names = ['chat_id', 'message', 'random_id']  # для ответа в лс должно быть peer_id
 
-                # inf_about_title = [*info['name'], str(info['description']), *list(titleInMy.values())]
+                params = [str(for_id)[-1], '\n--------------------\n'.join(inf_about_title),
+                          random.randint(-2147483648, +2147483648)]
 
-                # names = ['chat_id', 'message', 'random_id'] # для ответа в лс должно быть peer_id
-
-                # params = [str(for_id)[-1], '\n--------------------\n'.join(inf_about_title), random.randint(-2147483648, +2147483648)]
-
-                # Api.api('messages.send', names, params)
+                Api.api('messages.send', names, params)
 
                 rpS += 5
+            # if input_message1[0] == 'в список':
 
-        if len(message) == 0:
+        if len(taken_message) == 0:
             names = ['chat_id', 'attachment', 'random_id']
             params = [str(for_id)[-1], Api.forMessage(attc), random.randint(-2147483648, +2147483648)]
             Api.api('messages.send', names, params)
@@ -41,29 +47,29 @@ def message(event):
     start = time.time()
     if event['type'] == 'message_new':
         forid = event["object"]['peer_id']
+        current[event["object"]['from_id']] = 0
         input_message = event["object"]['text'].lower()
         att = (event["object"])['attachments']
         if (len(att) == 0) and (len(event["object"]['fwd_messages']) != 0):
-            object_find_and_send(forid, rps, input_message, event["object"]['fwd_messages'][0]['attachments'])
+            att = event["object"]['fwd_messages'][0]['attachments']
+            object_find_and_send(forid, rps, current, input_message, att)
         else:
-            object_find_and_send(forid, rps, input_message, att)
+            object_find_and_send(forid, rps, current, input_message, att)
         print(datetime.now())
         print(time.time() - start)
 
 
+current = {}
 while True:
     try:
-        a = Api.longPoll()
-        while len(a[1]) == 0:
-            print('noevents')
-            a = Api.longPoll()
-        print(len(a[1]))
-        event = (a[1])[0]
+        a = api.longPoll()
+        while len(a) == 0:
+            a = api.longPoll()
+        event = a[0]
+        print(event)
         for i in range(1):
-            my_thread = threading.Thread(target=message, args=(event,))
+            my_thread = threading.Thread(target=message, daemon=True, args=(event,))
             my_thread.start()
 
-
-
-    except Exception:
-        print(Exception)
+    except Exception as e:
+        print(str(e))
